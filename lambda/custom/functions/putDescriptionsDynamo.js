e7013f0f7b9361afcf1b17ee5aed335a21bdfa7f
MARX <admin@marx.tech>
2018-05-02 12:35:47 -0400

    commit

---
+++ lambda/custom/functions/putDescriptionsDynamo.js
@@ -0,0 +1,30 @@
+  
+  const getDescriptionData = function (symbol) { //,cb) {
+          
+      var AWS = require("aws-sdk");
+  
+      AWS.config.update({
+      region: "us-east-1"
+      });
+  
+      var docClient = new AWS.DynamoDB.DocumentClient()
+      var table = "marx-trader-coin-descriptions";
+  
+      var params = {
+          TableName: table,
+          Key:{
+              "symbol": symbol,
+              "type" : 'currency'
+          }
+      };
+  
+      docClient.get(params, function(err, data) {
+          if (err) {
+              console.error("Error ; ", JSON.stringify(err, null, 2));
+              return 
+          } else {
+             console.log(JSON.stringify(data.Item.description))
+          }
+      });
+  }
+  getDescriptionData('btcusd')
